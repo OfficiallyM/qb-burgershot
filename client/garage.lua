fc51ee24149c64754693d0b64ef25357648228a3
Kyle Brasenell <miner717@hotmail.com>
2022-04-11 21:26:06 +0100

    Update garage.lua to fix vehicle spawning

--- client/garage.lua
+++ client/garage.lua
@@ -62,25 +62,25 @@ function loadAnimDict( dict )
 end
 
 RegisterNetEvent('qb-burgershot:garage')
-AddEventHandler('qb-burgershot:garage', function(bs)
-  local vehicle = bs.vehicle
+AddEventHandler('qb-burgershot:garage', function(vehicle)
   local coords = { ['x'] = -1172.861, ['y'] = -888.4072, ['z'] = 13.940833 -1, ['h'] = 40.516719 }
   QBCore.Functions.GetPlayerData(function(PlayerData)
     if PlayerData.job.name == "burgershot" then
       if vehicle == 'stalion2' then
-      
+        print(vehicle)
         QBCore.Functions.SpawnVehicle(vehicle, function(veh)
-          SetVehicleNumberPlateText(veh, "BURGER"..tostring(math.random(1000, 9999)))
+          local plate = "BURGER"..tostring(math.random(1000, 9999))
+          SetVehicleNumberPlateText(veh, plate)
           exports['LegacyFuel']:SetFuel(veh, 100.0)
           SetEntityHeading(veh, coords.h)
           TaskWarpPedIntoVehicle(GetPlayerPed(-1), veh, -1)
-          TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(veh))
+          SetEntityAsMissionEntity(veh, true, true)
+          TriggerEvent("vehiclekeys:client:SetOwner", plate)
           SetVehicleEngineOn(veh, true, true)
         end, coords, true)
       end
     else 
       QBCore.Functions.Notify('You are not an employee of BurgerShot.', 'error')
-      
     end
   end)    
 end)
@@ -105,9 +105,7 @@ RegisterNetEvent('garage:BurgerShotGarage', function()
       header = "Stallion",
       context = "Declasse Burger Shot Stallion",
       event = "qb-burgershot:garage",
-      args = {
-        vehicle = 'stalion2',
-      },
+      args = {'stalion2'},
     },
     {
       header = "Store Vehicle",
